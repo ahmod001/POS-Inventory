454b29420e778b8ac5c6898ba06c1c91de1f2ceb
ahmod001 <ahmodh08@gmail.com>
2023-10-01 08:19:56 +0600

    (#18)

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,72 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\categoryController;
+use App\Http\Controllers\customerController;
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\invoiceController;
+use App\Http\Controllers\productController;
+use App\Http\Controllers\reportController;
+use App\Http\Controllers\userController;
+use App\Http\Middleware\tokenVerficationMiddleware;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Dashboard
+Route::controller(dashboardController::class)->group(function () {
+    Route::get('/total-customer', 'totalCustomer')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/total-product', 'totalProduct')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/total-category', 'totalCategory')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/total-sale', 'totalSale')->middleware(tokenVerficationMiddleware::class);
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// User
+Route::controller(userController::class)->group(function () {
+    Route::post('/user-register', 'userRegistration');
+    Route::post('/user-login', 'userLogin');
+    Route::post('/send-otp', 'sendOTPCode');
+    Route::post('/verify-otp', 'verifyOTPCode');
+    Route::get('/user-logout', 'userLogOut');
+
+    // Verify Token
+    Route::post('/reset-password', 'resetPassword')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/user-profile-details', 'userProfile')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/user-update', 'userUpdate')->middleware(tokenVerficationMiddleware::class);
+});
+
+// Category
+Route::controller(categoryController::class)->group(function () {
+    Route::post('/create-category', 'createCategory')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/update-category', 'updateCategory')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/delete-category', 'deleteCategory')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/category-list', 'categoryList')->middleware(tokenVerficationMiddleware::class);
+});
+
+// Customer
+Route::controller(customerController::class)->group(function () {
+    Route::post('/create-customer', 'createCustomer')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/update-customer', 'updateCustomer')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/delete-customer', 'deleteCustomer')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/customer-list', 'customerList')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/customers/{id}', 'customerById')->middleware(tokenVerficationMiddleware::class);
+});
+
+// Product
+Route::controller(productController::class)->group(function () {
+    Route::post('/create-product', 'createProduct')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/update-product', 'updateProduct')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/delete-product', 'deleteProduct')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/product-list', 'productList')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/products/{id}', 'productById')->middleware(tokenVerficationMiddleware::class);
+});
+
+// Invoice
+Route::controller(invoiceController::class)->group(function () {
+    Route::post('/create-invoice', 'createInvoice')->middleware(tokenVerficationMiddleware::class);
+    Route::get('/invoice-list', 'invoiceList')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/invoice-details', 'invoiceDetails')->middleware(tokenVerficationMiddleware::class);
+    Route::post('/delete-invoice', 'deleteInvoice')->middleware(tokenVerficationMiddleware::class);
+});
+
+// Report
+Route::controller(reportController::class)->group(function () {
+    Route::get('/sales-report/{from}/{to}', 'salesReport')->middleware(tokenVerficationMiddleware::class);
 });
